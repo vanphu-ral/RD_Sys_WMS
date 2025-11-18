847f13ac03eae646f7ef9cc64086416a827d0315
nkhieu215 <102019148+nkhieu215@users.noreply.github.com>
2025-11-18 13:48:36 +0700

    Update config.py

--- BE/app/core/config.py
+++ BE/app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     # DATABASE_URL: str = "mssql+aioodbc://sa:Admin!112358@localhost:1433/SYS_WMS_DB?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
     # DATABASE_URL_2: str = "mssql+aioodbc://ral_wms:Ral%402804%40@192.168.21.61:1433/BANHANG_Thang6?driver=ODBC+Driver+17+for+SQL+Server"
     DATABASE_URL_2: str = "mssql+aioodbc:///?odbc_connect=DRIVER%3D%7BODBC+Driver+18+for+SQL+Server%7D%3BSERVER%3D192.168.21.61%2C1433%3BDATABASE%3DBANGHANG_Thang6%3BUID%3Dral_wms%3BPWD%3DRal%402804%40%3BEncrypt%3Dno%3BTrustServerCertificate%3Dyes"
-    DATABASE_URL: str = "postgresql+asyncpg://admin:admin123@localhost:5432/WMS_DB"
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:admin123@localhost:5432/WMS_DB"
 
     SECRET_KEY: str = "secret-key"
     ALGORITHM: str = "HS256"
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
 # clientID : RD_KHO
 
     # CORS
-    ALLOWED_ORIGINS: List[str] = ["http://localhost:4200", "http://192.168.20.101:4200","http://localhost:9004", "http://192.168.20.101:9004" ]
+    ALLOWED_ORIGINS: List[str] = ["http://localhost:4200", "http://192.168.20.101:4200","http://localhost:9004", "http://192.168.20.101:9004","https://ral.wms-logistic.rangdong.com.vn:9004" ]
 
     LOG_LEVEL: str = "INFO"
 
