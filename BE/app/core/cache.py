71bb0a0b83277f4d269a28be1f3ba89df6bbf529
lean <xuanlvph52216@gmail.com>
2026-01-04 08:40:04 +0700

    update v2.3

---
+++ BE/app/core/cache.py
@@ -0,0 +1,16 @@
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
+from app.core.config import settings
+
+async def init_cache():
+    redis = aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+        password=settings.REDIS_PASSWORD,
+        db=settings.REDIS_DB
+    )
+    FastAPICache.init(RedisBackend(redis), prefix="wms-cache")
+
+async def close_cache():
+    await FastAPICache.clear()
